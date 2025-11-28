fe23b2581e98ae618f7e320711739c9e8554b67b
fatimarqz9 <fam04marquez@gmail.com>
2025-11-27 22:50:24 -0600

    creacion del formulario para crear motoristas

--- gestionEnvios/routes/web.php
+++ gestionEnvios/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\MotoristasController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home.index');
 });
+
+Route::resource("motoristas", MotoristasController::class);
