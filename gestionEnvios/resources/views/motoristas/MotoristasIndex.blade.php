56688ed3c29be1310abfae40819a40e264b493de
fatimarqz9 <fam04marquez@gmail.com>
2025-11-29 15:11:25 -0600

    Agregando notificaciones con Toastify

--- gestionEnvios/resources/views/motoristas/MotoristasIndex.blade.php
+++ gestionEnvios/resources/views/motoristas/MotoristasIndex.blade.php
@@ -1,5 +1,12 @@
 @extends('home.index')
+@section('title', 'UES - Gestión de Motoristas')
+
+
+@section('styles')
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
+@endsection
 @section('content')
+
     @livewire('motoristas.motoristas')
     @section('scripts')
     <script src="{{ asset('vendor/livewire/livewire.js') }}" data-navigate-once></script>
@@ -16,5 +23,34 @@
         // Iniciar Livewire manualmente
         //Livewire.start();
     </script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
+    <script>
+    function showToast(message, type = 'success') {
+        const colors = {
+            success: 'linear-gradient(to right, #00b09b, #96c93d)',
+            error: 'linear-gradient(to right, #ff5f6d, #ffc371)',
+            warning: 'linear-gradient(to right, #f7b733, #fc4a1a)',
+            info: 'linear-gradient(to right, #4facfe, #00f2fe)'
+        };
+
+        Toastify({
+            text: message,
+            duration: 3000,
+            newWindow: true,
+            close: true,
+            gravity: "top",
+            position: "right",
+            backgroundColor: colors[type],
+            stopOnFocus: true,
+            className: "toastify-custom",
+        }).showToast();
+    }
+
+    document.addEventListener('livewire:init', () => {
+        Livewire.on('toast', (event) => {
+            showToast(event.message, event.type);
+        });
+    });
+</script>
     @endsection
 @endsection
