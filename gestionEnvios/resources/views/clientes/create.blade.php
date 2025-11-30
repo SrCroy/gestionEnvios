a3e805ebbc3be0d40d7f4486ecdc975acb5d9229
Adán Omar Quevedo Argueta <adanque12dst@gmail.com>
2025-11-30 09:18:05 -0600

    subida en seco, sin probar nada
    subida asi copiados y pegados los que tranbaje en otras ramas en lo que resuelvo lo del docker

---
+++ gestionEnvios/resources/views/clientes/create.blade.php
@@ -0,0 +1,25 @@
+@extends('home.app')
+
+@section('title', 'Crear Cliente - UES FMO')
+
+@section('content')
+    <form action="{{ route('clientes.create') }}" method="post">
+        @csrf
+        <label for="">Nombre</label>
+        <input type="text" id="nombre" name="nombre" class="w-full"><br>
+        <label for="">Direccion</label>
+        <input type="text" id="direccion" name="direccion" class="w-full"><br>
+        <label for="">Email</label>
+        <input type="email" id="email" name="email" class="w-full"><br>
+        <label for="">telefono</label>
+        <input type="text" id="telefono" name="telefono" class="w-full"><br>
+        <label for="">Latitud</label>
+        <input type="number" step="0.00001" name="latitud" id="latitud" class="w-full"><br>
+        <label for="">Longitud</label>
+        <input type="number" step="0.00001" name="longitud" id="longitud" class="w-full"><br>
+        <button type="submit" class="btn btn-success">
+            crear
+        </button>
+    </form>
+    <a href={{ route('clientes.index') }} class="btn btn-danger">Regresar</a>
+@endsection
---
+++ gestionEnvios/resources/views/clientes/show.blade.php
@@ -0,0 +1,32 @@
+@extends('home.app')
+
+@section('title', 'Detalles del Cliente - UES FMO')
+
+@section('content')
+    <div class="flex flex-col gap-2 p-2 bg-secondary container">
+        <div class="row">
+            <div class="col-2 text-info">
+                <h1>id: </h1>
+                <h1>nombre: </h1>
+                <h1>email: </h1>
+                <h1>telefono: </h1>
+                <h1>direccion: </h1>
+                <h1>latitud: </h1>
+                <h1>longitud: </h1>
+            </div>
+            <div class="col-8">
+                <h1 class="text-light">{{ $cliente->id }}</h1>
+                <h1 class="text-light">{{ $cliente->nombre }}</h1>
+                <h1 class="text-light">{{ $cliente->email }}</h1>
+                <h1 class="text-light">{{ $cliente->telefono }}</h1>
+                <h1 class="text-light">{{ $cliente->direccion }}</h1>
+                <h1 class="text-light">{{ $cliente->latitud }}</h1>
+                <h1 class="text-light">{{ $cliente->longitud }}</h1>
+            </div>
+        </div>
+    </div>
+    <div class="d-flex flex-row justify-content-around p-2 bg-info">
+        <a href={{ route('clientes.edit', $cliente->id) }} class="btn btn-success">edit</a>
+        <a href={{ route('clientes.delete', $cliente->id) }} class="btn btn-danger">delite</a>
+    </div>
+@endsection
